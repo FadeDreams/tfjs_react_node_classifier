b1bc7a015b981185f240a481f4b1d339e266800f
white <whitemres@gmail.com>
2021-10-27 10:45:10 +0330

    search functionalities done

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,61 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from .models import Car
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 def cars(request):
-    return render(request,'cars/cars.html')
+    cars=Car.objects.order_by('-created_date')
+    paginator=Paginator(cars,1)
+    page=request.GET.get('page')
+    paged_cars=paginator.get_page(page)
+    data={
+        'cars':paged_cars
+    }
+    return render(request,'cars/cars.html',data)
+
+def car_detail(request,id):
+    single_car = get_object_or_404(Car,pk=id)
+    data={
+        'single_car':single_car,
+    }
+    return render(request,'cars/car_detail.html',data)
+
+def search(request):
+    cars=Car.objects.order_by('-created_date')
+    if( 'keyword' in request.GET ):
+        keyword=request.GET.get('keyword')
+        if keyword:
+            cars=cars.filter(description__icontains=keyword)
+    
+    if( 'model' in request.GET ):
+        model=request.GET.get('model')
+        if model:
+            cars=cars.filter(model__iexact=model)
+          
+    if( 'city' in request.GET ):
+        city=request.GET.get('city')
+        if city:
+            cars=cars.filter(city__iexact=city)
+            
+    if( 'year' in request.GET ):
+        year=request.GET.get('year')
+        if year:
+            cars=cars.filter(year__iexact=year)
+            
+    if( 'body_style' in request.GET ):
+        body_style=request.GET.get('body_style')
+        if body_style:
+            cars=cars.filter(body_style__iexact=body_style)
+            
+    if('min_price' in request.GET ):        
+        min_price=request.GET['min_price']
+    
+    if('max_price' in request.GET ):
+        max_price=request.GET['max_price']
+        if max_price and min_price:
+            cars.filter(price__gte=min_price,price__lte=max_price)
+            
+    
+    data={
+        'cars':cars,
+    }
+    return render(request,'cars/search.html',data)
--- pages/views.py
+++ pages/views.py
@@ -6,11 +6,28 @@ def home(request):
     teams=Team.objects.all()
     featured_cars=Car.objects.order_by('-created_date').filter(is_featured=True)
     latest_cars=Car.objects.order_by('-created_date')
+    #The values() method returns a QuerySet containing dictionaries:
+    #<QuerySet [{'comment_id': 1}, {'comment_id': 2}]>
+    # search_fields=Car.objects.values('model','city','year','body_style')
+    #he values_list() method returns a QuerySet containing tuples:
+    #<QuerySet [(1,), (2,)]>
+    #If you are using values_list() with a single field,
+    # you can use flat=True to return a QuerySet of single values instead of 1-tuples:
+    #<QuerySet [1, 2]>
+
+    model_search=Car.objects.values_list('model',flat=True).distinct()
+    city_search=Car.objects.values_list('city',flat=True).distinct()
+    year_search=Car.objects.values_list('year',flat=True).distinct()
+    body_style_search=Car.objects.values_list('body_style',flat=True).distinct()
     data={
         'teams':teams,
         'featured_cars':featured_cars,
-        'latest_cars':latest_cars
-        
+        'latest_cars':latest_cars,
+        # 'search_fields':search_fields,
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style_search':body_style_search,
     }
     return render(request,'pages/home.html',data)
 
